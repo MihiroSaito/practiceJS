ea6ee0af4bc8dee689d785301b55b339ae74230f
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-06-15 07:45:20 +0900

    add images

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,3 +1,37 @@
+var imageList = [
+  "assets/cave.jpg",
+  "assets/GrandCanyon2.jpg",
+  "assets/lake.jpg",
+  "assets/lake2.jpg",
+  "assets/rock.jpg",
+  "assets/rock2.jpg",
+  "assets/rock3.jpg",
+  "assets/tree.jpg",
+  "assets/waterfall.jpg",
+  "assets/waterfall2.jpg"
+]
 
+for(var i = 0; i < imageList.length; i++) {
 
+  var slide = document.createElement("li");
 
+  slide.innerHTML = "<img src='"+ imageList[i] + "'>";
+
+  document.getElementsByClassName("images")[0].appendChild(slide);
+
+  var point = document.createElement("li");
+
+  point.setAttribute("data-point-index", i)
+
+  document.getElementsByClassName("points")[0].appendChild(point);
+}
+
+var image = document.getElementsByClassName("images")[0].getElementsByTagName("li");
+
+var point = document.getElementsByClassName("points")[0].getElementsByTagName("li");
+
+var nowIndex = 0;
+
+image[nowIndex].classList.add("show");
+
+point[nowIndex].classList.add("current");
